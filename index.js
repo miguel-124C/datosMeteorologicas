aa7d0fa45d6fad20336edd3ed263bcd50283763e
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2023-12-03 23:46:09 -0400

    Se añade spinner de carga

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const API_KEY = "758081342dee422aef49a98acf5d54cc";
 
 let horario = new Date();
-let numDays = horario.getDay();
+let numberDayACtual = horario.getDay();
 let days = ["Domingo","Lunes","Martes","Miércolres","Jueves","Viernes","Sábado"];
 let daysData = [{},{},{},{},{}];
 let minTemp = [];
@@ -13,29 +13,34 @@ const tempMax = document.querySelector(".temp-max");
 
 const fiveDays = document.querySelector(".temp-pronostico");
 
+const modalLoading = document.querySelector(".modal-loading");
+
 const calcularMinTemp=(tempMin,tempMax)=>{
     let min = 999;
     let max = -999;
 
-    for(const mins of tempMin)if(mins < min) min = mins;
+    for(const mins of tempMin)
+        if(mins < min) min = mins;
 
-    for(const maxs of tempMax)if(maxs > max) max = maxs;
+    for(const maxs of tempMax)
+        if(maxs > max) max = maxs;
 
     return [min,max];
 }
-const mostrarPronosticoFiveDays=(data)=>{
-    numDays++
-    if(numDays == 7) numDays = 0;
+const mostrarPronosticoFiveDays=(tempMinMax)=>{
+    numberDayACtual++
+    if(numberDayACtual == 7) numberDayACtual = 0;
     const divMinMaxTemp = document.createElement("div");
     divMinMaxTemp.classList.add("nexts-days");
     divMinMaxTemp.innerHTML = `
-        <span class="day">${days[numDays]}</span>
-        <div>min: ${Math.round(data[0])}º</div>
-        <div>max: ${Math.round(data[1])}º</div>
+        <span class="day">${days[numberDayACtual]}</span>
+        <div>min: ${Math.round(tempMinMax[0])}º</div>
+        <div>max: ${Math.round(tempMinMax[1])}º</div>
     `;
     fiveDays.appendChild(divMinMaxTemp);
 }
 const pronostico =(list)=>{
+    modalLoading.style.display = 'none';
     let limit = 8;
     let iterator = 0;
     for(let i=1;i<list.length;i++){
@@ -52,8 +57,7 @@ const pronostico =(list)=>{
     }
 }
 
-const mostrarTempAct=(dataTemp)=>{
-    console.log(dataTemp);
+const showTempActual=(dataTemp)=>{
     const {temp,temp_min,temp_max,feels_like,humidity,pressure} = dataTemp.main;
     tempAct.textContent = Math.round(temp)+"º";
     tempMin.textContent = "min: "+temp_min+"º";
@@ -65,13 +69,27 @@ const mostrarTempAct=(dataTemp)=>{
     document.querySelector(".description").textContent += dataTemp.weather[0].description;
 }
 
+
+const getForecast =( latitude, longitude )=>{
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
+    .then(Response=>Response.json())
+    .then(result => pronostico(result.list));
+}
+
+function getWeatherActuality( latitude, longitude ){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
+    .then(Response=>Response.json())
+    .then(result => showTempActual(result));
+}
+
+async function getApi(latitude, longitude ){
+    await getWeatherActuality(latitude, longitude );
+    await getForecast(latitude, longitude );
+}
+
 navigator.geolocation.getCurrentPosition(position=>{
     const {latitude,longitude} = position.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
-        .then(Response=>Response.json())
-        .then(result => pronostico(result.list));
-    
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
-        .then(Response=>Response.json())
-        .then(result => mostrarTempAct(result));
+
+    modalLoading.style.display = 'flex';
+    getApi( latitude, longitude );
 });    
